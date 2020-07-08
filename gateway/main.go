a8710c642a7db0775b612c9393bc2ef390ba4293
moguang <11@imooc.com>
2020-07-08 23:47:01 +0800

    更新测试接口

--- gateway/main.go
+++ gateway/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -14,26 +15,27 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func testInsert(h3Index string, lat float64, lon float64) {
+func testInsert(resolution int, lat float64, lon float64) {
 	dbCli := dbproxy.MongoConn()
 	// 指定获取要操作的数据集
 	collection := dbCli.Database("driverInfo").Collection("geoInfo")
 
+	geo := h3.GeoCoord{
+		Latitude:  lat,
+		Longitude: lon,
+	}
+	h3Index := h3.FromGeo(geo, resolution)
+	h3IndexStr := fmt.Sprintf("%#x", h3Index)
+
 	d1 := model.Driver{
 		Name:    "test1",
 		UID:     "123456",
-		H3Index: h3Index,
-		// GeoType: "Point",
-		// Coord:   []float64{lon, lat},
+		H3Index: h3IndexStr,
 		GeoInfo: bson.M{
 			"type":        "Point",
 			"coordinates": []float64{lon, lat},
 		},
 	}
-	// d1Json, err := json.Marshal(d1)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
 
 	insertResult, err := collection.InsertOne(context.TODO(), d1)
 	if err != nil {
@@ -43,7 +45,7 @@ func testInsert(h3Index string, lat float64, lon float64) {
 	fmt.Println("Inserted a single document: ", insertResult.InsertedID)
 }
 
-func testQuery(lat float64, lon float64) {
+func testQuery(lat float64, lon float64) (res []model.Driver, err error) {
 	dbCli := dbproxy.MongoConn()
 	// 指定获取要操作的数据集
 	collection := dbCli.Database("driverInfo").Collection("geoInfo")
@@ -68,15 +70,17 @@ func testQuery(lat float64, lon float64) {
 	}
 	for filterCursor.Next(context.TODO()) {
 		var elem model.Driver
-		err := filterCursor.Decode(&elem)
+		err = filterCursor.Decode(&elem)
 		if err != nil {
 			log.Fatal(err)
+			return
 		}
-		log.Printf("%+v\n", elem)
+		res = append(res, elem)
 	}
+	return
 }
 
-func testHandler(w http.ResponseWriter, r *http.Request) {
+func testInsertHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
 	lat, err1 := strconv.ParseFloat(r.Form.Get("lat"), 64)
@@ -86,31 +90,59 @@ func testHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	geo := h3.GeoCoord{
-		Latitude:  lat, // 37.775938728915946,
-		Longitude: lon, // -122.41795063018799,
-	}
-	resolution := 9
+	resolution := 7
 
-	h3Index := h3.FromGeo(geo, resolution)
-	h3IndexStr := fmt.Sprintf("%#x", h3Index)
-	// Output:
-	// 0x8928308280fffff
-
-	neighbors := h3.KRing(h3Index, 1)
-	for _, n := range neighbors {
-		fmt.Printf("%#x\n", n)
+	for i := 1.0; i <= 10.0; i++ {
+		var delta = 0.01 * i
+		testInsert(resolution, lat+delta, lon+delta)
+		testInsert(resolution, lat+delta, lon-delta)
+		testInsert(resolution, lat-delta, lon+delta)
+		testInsert(resolution, lat-delta, lon-delta)
 	}
 
-	// // test mongo
-	// testInsert(h3IndexStr, lat, lon)
-	testQuery(lat+0.1, lon+0.1)
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	res := fmt.Sprintf(`{"code":0}`)
+	w.Write([]byte(res))
+}
 
-	w.Write([]byte(h3IndexStr))
+func testQueryHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	r.ParseForm()
+
+	lat, err1 := strconv.ParseFloat(r.Form.Get("lat"), 64)
+	lon, err2 := strconv.ParseFloat(r.Form.Get("lon"), 64)
+	if err1 != nil || err2 != nil {
+		w.Write([]byte("FAILED"))
+		return
+	}
+
+	// neighbors := h3.KRing(h3Index, 1)
+	// for _, n := range neighbors {
+	// 	fmt.Printf("%#x\n", n)
+	// }
+
+	// 经度0.1度约10km, 纬度0.1度约11.1km
+	drivers, err := testQuery(lat+0.1, lon+0.1)
+	if err != nil {
+		res := fmt.Sprintf(`{"code":-1}`)
+		w.Write([]byte(res))
+		return
+	}
+
+	data, err := json.Marshal(drivers)
+	if err != nil {
+		res := fmt.Sprintf(`{"code":-2}`)
+		w.Write([]byte(res))
+		return
+	}
+
+	res := fmt.Sprintf(`{"code":0,"count":%d,"data":%s}`, len(drivers), string(data))
+	w.Write([]byte(res))
 }
 
 func main() {
-	http.HandleFunc("/test", testHandler)
+	http.HandleFunc("/test/insert", testInsertHandler)
+	http.HandleFunc("/test/query", testQueryHandler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println(err.Error())
--- model/driver.go
+++ model/driver.go
@@ -4,10 +4,8 @@ import "go.mongodb.org/mongo-driver/bson"
 
 // Driver : 司机结构体
 type Driver struct {
-	Name    string `bson:"name"`
-	UID     string `bson:"uid"`
-	H3Index string `bson:"h3index"`
-	// GeoType string    `bson:"type"`
-	// Coord   []float64 `bson:"coordinates"`
-	GeoInfo bson.M `bson:"geoinfo"`
+	Name    string `json:"name" bson:"name"`
+	UID     string `json:"uid" bson:"uid"`
+	H3Index string `json:"h3index" bson:"h3index"`
+	GeoInfo bson.M `json:"geoinfo" bson:"geoinfo"`
 }
