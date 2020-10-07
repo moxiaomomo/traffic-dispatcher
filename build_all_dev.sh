7cec7702bfb978fba70f5e6bb827647336f8007a
moxiaomomo <moxiaomomo@gmail.com>
2020-10-07 23:16:26 +0800

    Add build app script

---
+++ build_all_dev.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+go build -o bin/svc_user service/user/main.go
+go build -o bin/svc_order service/order/main.go
+go build -o bin/svc_lbs service/lbs/main.go
+go build -o bin/api_driver api/driver/main.go
+go build -o bin/api_passenger api/passenger/main.go
