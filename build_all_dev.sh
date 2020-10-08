0dfd8a1a0b806c684fb16f0ec8ae4e0714492463
moxiaomomo <moxiaomomo@gmail.com>
2020-10-08 08:55:09 +0800

    Update build script

--- build_all_dev.sh
+++ build_all_dev.sh
@@ -4,3 +4,4 @@ go build -o bin/svc_order service/order/main.go
 go build -o bin/svc_lbs service/lbs/main.go
 go build -o bin/api_driver api/driver/main.go
 go build -o bin/api_passenger api/passenger/main.go
+go build -o bin/web_geo web/geo/main.go
